e43353af80805286ee6b1d85c712b8dccea77398
mahogey <mahogey.games@gmail.com>
2021-03-06 17:25:51 -0800

    Create Open and Save options for game.

---
+++ src/main/kotlin/main/Utilities.kt
@@ -0,0 +1,22 @@
+package main
+
+import com.google.gson.GsonBuilder
+import java.io.File
+
+fun writeObjectToFileSystem( obj : Any, file : File ) {
+    val gson = GsonBuilder().setPrettyPrinting().create()
+    val json = gson.toJson( obj )
+    file.printWriter().use { out ->
+        out.println( json )
+    }
+}
+
+fun readObjectFromFileSystem( file : File ) : String {
+    var json : String = "";
+    file.useLines { lines ->
+        lines.forEach {
+            json += it
+        }
+    }
+    return json
+}
